237bf1d0e59c4c56581478445509cac10058042d
Daniel Xu <>
2019-10-04 14:34:20 +1000

    1st commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView sydneyTime=findViewById(R.id.textViewTimeSydney);
+        sydneyTime.setText(set24Time("Australia/Sydney"));
+
+
+        TextView parisTime=findViewById(R.id.textViewTimeParis);
+        parisTime.setText(set24Time("Europe/Paris"));
+
+/*        TextView shanghaiTime=findViewById(R.id.textViewTimeShanghai);
+        shanghaiTime.setText(set24Time("Asia/Shanghai"));
+
+        TextView nyTime=findViewById(R.id.textViewTimeNY);
+        nyTime.setText(set24Time("America/New_York"));
+
+        TextView tokyoTime=findViewById(R.id.textViewTimeTokyo);
+        tokyoTime.setText(set24Time("Asia/Tokyo"));
+*/
+
+
+
+    }
+
+    public void hour12Button(View view){
+        TextView sydneyTime=findViewById(R.id.textViewTimeSydney);
+        sydneyTime.setText(set12Time("Australia/Sydney"));
+
+        TextView parisTime=findViewById(R.id.textViewTimeParis);
+        parisTime.setText(set12Time("Europe/Paris"));
+
+/*        TextView shanghaiTime=findViewById(R.id.textViewTimeShanghai);
+        shanghaiTime.setText(set12Time("Asia/Shanghai"));
+
+        TextView nyTime=findViewById(R.id.textViewTimeNY);
+        nyTime.setText(set12Time("America/New_York"));
+
+        TextView tokyoTime=findViewById(R.id.textViewTimeTokyo);
+        tokyoTime.setText(set12Time("Asia/Tokyo"));
+
+*/
+    }
+
+    public void hour24Button(View view){
+        TextView sydneyTime=findViewById(R.id.textViewTimeSydney);
+        sydneyTime.setText(set24Time("Australia/Sydney"));
+
+        TextView parisTime=findViewById(R.id.textViewTimeParis);
+        parisTime.setText(set24Time("Europe/Paris"));
+//
+//        TextView shanghaiTime=findViewById(R.id.textViewTimeShanghai);
+//        shanghaiTime.setText(set24Time("Asia/Shanghai"));
+//
+//        TextView nyTime=findViewById(R.id.textViewTimeNY);
+//        nyTime.setText(set24Time("America/New_York"));
+//
+//        TextView tokyoTime=findViewById(R.id.textViewTimeTokyo);
+//        tokyoTime.setText(set24Time("Asia/Tokyo"));
+    }
+
+    private String set12Time(String city){
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a z", Locale.getDefault());
+        sdf.setTimeZone(TimeZone.getTimeZone(city));
+        String currentTime = sdf.format(new Date());
+        return currentTime;
+    }
+
+    private String set24Time(String city){
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm z", Locale.getDefault());
+        sdf.setTimeZone(TimeZone.getTimeZone(city));
+        String currentTime = sdf.format(new Date());
+        return currentTime;
+    }
+}
